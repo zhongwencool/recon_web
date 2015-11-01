904873ac5f89409aa51160d7425adf1bc396168f
zhongwen <dengzhongwen@eryuapp.com>
2015-11-01 18:47:39 +0800

    update top with pause function

--- src/recon_web.erl
+++ src/recon_web.erl
@@ -11,9 +11,12 @@ start([debug]) ->
     application:ensure_all_started(recon_web),
     lager:set_loglevel(lager_console_backend, debug).
 
+%% @doc a top tool in erlang shell the reflushtime is Milliseconds
+-define(TOP_MIN_REFLUSH_INTERAL, 2000).
 -spec top() -> stop.
-top() -> top(2000).
+top() -> top(?TOP_MIN_REFLUSH_INTERAL).
 -spec top(pos_integer()) -> stop.
-top(ReflushTime) ->
-  Pid = spawn_link(fun() -> recon_web_top:loop(ReflushTime) end),
+top(ReflushMillSecond)when ReflushMillSecond >= ?TOP_MIN_REFLUSH_INTERAL ->
+  Pid = spawn_link(fun() -> recon_web_top:loop(ReflushMillSecond) end),
   recon_web_top:top(Pid).
+
